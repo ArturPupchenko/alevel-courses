e63a4cd0320f844a73af6062e509a578a85795ce
arturpupchenko <PupchenkoArtur@gmail.com>
2020-09-04 19:00:35 +0300

    Deleted outdated modules.

--- jpabox/src/main/java/com/alevel/courses/jpabox/system/AbstractInitDbService.java
+++
@@ -1,5 +0,0 @@
-package com.alevel.courses.jpabox.system;
-
-public interface AbstractInitDbService {
-    void init();
-}
--- module3/src/main/java/com/alevel/courses/module3/jdbc/model/Income.java
+++
@@ -1,4 +0,0 @@
-package com.alevel.courses.module3.jdbc.model;
-
-public class Income extends Operation {
-}
